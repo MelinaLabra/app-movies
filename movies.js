0ca3ba4f3e7818c4b9ee35ffd44654924bd25f15
MelinaLabra <melina.lb04@gmail.com>
2023-01-20 14:44:14 -0300

    agrego app-movies

---
+++ movies.js
@@ -0,0 +1,49 @@
+let movies = [
+    {
+        id: 1 ,
+        title: "The avengers",
+        rating: 9,
+        awards: 15,
+        length: 120,
+        price: 150,
+        genre: "Acción",
+    },
+    {
+        id: 2,
+        title: "shrek 2",
+        rating: 6,
+        awards: 0,
+        length: 120,
+        price: 200,
+        genre: "animación",
+    },
+    {
+        id: 3,
+        title: "Son como niños" ,
+        rating: 5,
+        awards: 2,
+        length: 120,
+        price: 120,
+        genre: "Comedia",
+    },
+    {
+        id: 4 ,
+        title: "La invitación",
+        rating: 5,
+        awards: 1,
+        length: 120,
+        price: 500,
+        genre: "Suspenso",
+    },
+    {
+        id: 5 ,
+        title: "Harry Potter",
+        rating: 10,
+        awards: 8,
+        length: 150,
+        price: 150,
+        genre: "Ciencia ficción",
+    }
+]
+
+module.exports = movies;
